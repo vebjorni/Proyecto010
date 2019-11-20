85778d12aea1ac4e8da6977074f6efffbc7c8a28
vebjorni <raulsanchezgonzalez1@gmail.com>
2019-11-20 10:41:21 +0100

    Initial commit

---
+++ app/src/main/java/com/example/proyecto010/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.proyecto010;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void boton(View view){
+        Intent i = new Intent(this, boton.class);
+        startActivity(i);
+    }
+}
